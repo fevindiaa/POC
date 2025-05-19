196a4c7f2f878e63574392a9fd12f1b210beccf2
deepakfev <deepak_y@fev.com>
2025-05-20 01:09:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,11 @@
-
 terraform {
   required_providers {
     kafka = {
-      source  = "Mongey/kafka"
+      source  = "mongey/kafka"
       version = "0.9.0"
     }
   }
 }
-
 provider "kafka" {
   bootstrap_servers = ["boot-dxp692ko.c3.kafka-serverless.eu-north-1.amazonaws.com:9098"]
-
-  config = {
-    "security.protocol"        = "SSL"
-    "ssl.ca.location"          = "${path.module}/certs/ca.crt"
-    "ssl.certificate.location" = "${path.module}/certs/client.crt"
-    "ssl.key.location"         = "${path.module}/certs/client.key"
-  }
 }
