45d58dc3fe9dfea833f527202319f7be531d8609
deepakfev <deepak_y@fev.com>
2025-05-20 01:07:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,9 +10,11 @@ terraform {
 
 provider "kafka" {
   bootstrap_servers = ["boot-dxp692ko.c3.kafka-serverless.eu-north-1.amazonaws.com:9098"]
-  tls_enabled       = true
-  tls_ca_cert       = file("${path.module}/certs/ca.crt")
-  tls_cert          = file("${path.module}/certs/client.crt")
-  tls_key           = file("${path.module}/certs/client.key")
-  skip_tls_verify   = false
+
+  config = {
+    "security.protocol"        = "SSL"
+    "ssl.ca.location"          = "${path.module}/certs/ca.crt"
+    "ssl.certificate.location" = "${path.module}/certs/client.crt"
+    "ssl.key.location"         = "${path.module}/certs/client.key"
+  }
 }
